84a317b26a7403cb555fdca5755d573c32d9c640
Soumya Deep saha <111523309+SoumyaDeepSaha1200@users.noreply.github.com>
2025-08-15 23:18:03 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -117,11 +117,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+# URL to access static files
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # or your custom static files directory
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for production
+
+# Folder to collect static files in production
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optional: Additional static folders
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
